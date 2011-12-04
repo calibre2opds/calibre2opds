8419a2a63f690daa6f3e3218448a59bc5e81ad42
itimpi@gmail.com <itimpi@gmail.com@6ab1c9ca-8b3c-d243-0dc9-87497c800d1a>
2011-12-04 22:17:24 +0000

    Further fix for using External icon files (previous fix broke using some of the internal icons)

--- trunk/OpdsOutput/src/main/java/com/gmail/dpierron/calibre/opds/BooksSubCatalog.java
+++ trunk/OpdsOutput/src/main/java/com/gmail/dpierron/calibre/opds/BooksSubCatalog.java
@@ -333,10 +333,12 @@ public abstract class BooksSubCatalog extends SubCatalog {
       List<Element> result;
       if (willSplitByDate) {
         Breadcrumbs breadcrumbs = Breadcrumbs.addBreadcrumb(pBreadcrumbs, title, urlExt);
-        result = getListOfBooksSplitByDate(breadcrumbs, mapOfBooksByDate, title, urn, pFilename, "../" + icon, options);
+        result = getListOfBooksSplitByDate(breadcrumbs, mapOfBooksByDate, title, urn, pFilename,
+                                (ConfigurationManager.INSTANCE.getCurrentProfile().getExternalIcons() ? "../" : "")  + icon, options);
       } else if (willSplitByLetter) {
         Breadcrumbs breadcrumbs = Breadcrumbs.addBreadcrumb(pBreadcrumbs, title, urlExt);
-        result = getListOfBooksSplitByLetter(breadcrumbs, mapOfBooksByLetter, title, urn, pFilename, SplitOption.SplitByLetter, "../" + icon, options);
+        result = getListOfBooksSplitByLetter(breadcrumbs, mapOfBooksByLetter, title, urn, pFilename, SplitOption.SplitByLetter,
+                                (ConfigurationManager.INSTANCE.getCurrentProfile().getExternalIcons() ? "../" : "") + icon, options);
       } else {
         result = new LinkedList<Element>();
         Breadcrumbs breadcrumbs = Breadcrumbs.addBreadcrumb(pBreadcrumbs, title, urlExt);
