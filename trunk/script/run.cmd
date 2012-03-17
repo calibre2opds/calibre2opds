bda17f684137710cbdd387d169cf0ac7fa3d529e
itimpi@gmail.com <itimpi@gmail.com@6ab1c9ca-8b3c-d243-0dc9-87497c800d1a>
2012-03-17 19:09:48 +0000

    Improvements to Windows Startup batch files to locate Java via registry to handle non-standard install locations. Shortcuts for Windows now run startup batch files in minimized mode to avoid unsightly DOS window showing. New localizations to support adding Test options in Gui for searches

--- trunk/script/run.cmd
+++ trunk/script/run.cmd
@@ -7,47 +7,116 @@ REM  that Java may run on some of those systems that do not
 REM  have Java on the search path but do have the JAVA_HOME
 REM  environment variable set or have installed to default location.
 
-:checkJava
-set _JAVACMD=%JAVACMD%
+
+cls
+echo Calibre2opds startup
+echo ====================
+echo '
+echo [INFO]  Trying to locate Java on this system
+echo [INFO]  ====================================
+
+set _C2O=OpdsOutput-3.1-SNAPSHOT.jar
+
 set _JAVAPROG=JAVA.EXE
 
+
+REM See if JAVA_HOME specifies location of Java (prefered method)
+
 if not "%JAVA_HOME%" == "" (
-  if exist "%JAVA_HOME%\bin\%_JAVAPROG%" (
+  if exist "%JAVA_HOME%\bin\%_JAVAPROG%"  (
     set _JAVACMD=%JAVA_HOME%\bin\%_JAVAPROG%
+    echo [INFO] Java found via JAVA_HOME evironment variable [%JAVA_HOME%]
     goto run_c2o
   )
 )
+echo [INFO] Java location not found via JAVA_HOME environment variable
 
-REM Check standard default 32-bit install locations
+REM Check default install locations
+
 if exist "%ProgramFiles%\Java\jre6\bin\%_JAVAPROG%" (
   set _JAVACMD=%ProgramFiles%\Java\jre6\bin\%_JAVAPROG%
+  echo [INFO] Java found via default 32-bit JRE6 location
+  echo [INFO] Java located at %ProgramFiles%\Java\jre6
   goto run_c2o
 )
-if exist "%ProgramFiles%\Java\jre7\bin\%_JAVAPROG%" (
-  set _JAVACMD=%ProgramFiles%\Java\jre7\bin\%_JAVAPROG%
+echo [INFO] Java not found in default 32-bit JRE6 location
+if exist "%ProgramFiles%\Java\jre7\bin\%JAVAPROG%" (
+  set _JAVACMD=%ProgramFiles%\Java\jre7\bin\%JAVAPROG%
+  echo [INFO] Java found at 32-bit JRE7 location [%ProgramFiles%\Java\jre7]
   goto run_c2o
 )
-
-REM Check standard default 64-bit install locations
+echo [INFO] Java not found in default 32-bit JRE7 location
 if exist "%ProgramFiles% (x86)\Java\jre6\bin\%_JAVAPROG%" (
-  set _JAVACMD=%ProgramFiles% (x86)\Java\jre6\bin\%_JAVAPROG%
+  set _JAVACMD="%ProgramFiles% (x86)\Java\jre6\bin\%_JAVAPROG%"
+  echo [INFO] Java found at default 64-bit JRE6 location [%ProgramFiles% ^(x86^)\Java\jre6]
   goto run_c2o
 )
+echo [INFO] Java not found in default 64-bit JRE6 location
 if exist "%ProgramFiles% (x86)\Java\jre7\bin\%_JAVAPROG%" (
-  set _JAVACMD=%ProgramFiles% (x86)\Java\jre7\bin\%_JAVAPROG%
+  set _JAVACMD="%ProgramFiles% (x86)\Java\jre7\bin\%_JAVAPROG%"
+  echo [INFO] Java found at default 64-bit JRE7 location [%ProgramFiles% ^(x86^)\Java\jre7]
   goto run_c2o
 )
+echo [INFO] Java not found in default 64-bit JRE7 location
 
 
+REM This next section is about trying to find Java home via the registry
+
+
+set _MYKEY=HKLM\Software\JavaSoft\Java RunTime Environment
+for /F "tokens=3" %%A IN ('REG.EXE QUERY "%_MYKEY%" /s ^| FIND "CurrentVersion"') DO set _MYVAR1=%%A
+if not "%_MYVAR1%" == "" (
+  echo [INFO] Java found via registry JRE key
+  goto get_javahome
+) 
+
+set _MYKEY=HKLM\Software\JavaSoft\Java Development Kit
+for /F "tokens=3" %%A IN ('REG.EXE QUERY "%_MYKEY%" /s ^| FIND "CurrentVersion"') DO set _MYVAR1=%%A
+if not "%_MYVAR1%" == "" (
+  echo [INFO] Java found via registry JDK key
+  goto get_javahome
+)
+set _MYKEY=HKLM\Software\Wow6432Node\JavaSoft\Java RunTime Environment
+for /F "tokens=3" %%A IN ('REG.EXE QUERY "%_MYKEY%" /s ^| FIND "CurrentVersion"') DO set _MYVAR1=%%A
+if not "%_MYVAR1%" == "" (
+  echo [INFO] Java found via registry JRE key for 32 bit Java on 64 bit system
+  goto get_javahome
+)
+set _MYKEY=HKLM\Software\Wow6432Node\JavaSoft\Java Development Kit  
+for /F "tokens=3" %%A IN ('REG.EXE QUERY "%_MYKEY%" /s ^| FIND "CurrentVersion"') DO set _MYVAR1=%%A
+if not "%_MYVAR1%" == "" (
+  echo [INFO] Java found via registry JDK key for 32 bit Java on 64 bit system
+  goto get_javahome
+)
+echo [INFO] Unable to find Java Registry entry
+goto java_notfound
+
+:get_javahome
+FOR /F "tokens=3*" %%A IN ('REG.EXE QUERY "%_MYKEY%\%_MYVAR1%" /s ^| FIND "JavaHome"') DO set _MYVAR2=%%A %%B
+if "%_MYVAR2%" == "" (
+  echo [INFO]  Failed to find JavaHome registry key
+  goto java_notfound
+)
+if not exist "%_MYVAR2%\bin\%_JAVAPROG%" (
+  echo [INFO]  Failed to find Java at location indicated by registry
+  goto java_notfound
+)
+set _JAVACMD=%_MYVAR2%\bin\%_JAVAPROG%
+echo [INFO] Java located at %_MYVAR2%
+goto run_c2o 
+
+:java_notfound
+echo '
+echo ----------------------------------------------------------------------------
 echo                                  WARNING
-echo
+echo '
 echo Unable to determine where the %JAVAPROG% program (which is part of Java) has
 echo been installed on your system.  Java does not appear to have been installed
-echo in any of the default locations.
+echo in any of the default locations or using the standard registry keys.
 echo '
-echo The recommended way to specify the location of Java is to set the JAVA_HOME
-echo environment variable. Normally the value of JAVA_HOME would be set to something
-echo like:
+echo In this case, the recommended way to proceed is to specify the location of Java
+echo by setting the JAVA_HOME environment variable. Normally the value of JAVA_HOME 
+echo would be set to something like:
 echo   JAVA_HOME=C:\Program Files\Java\jre6
 echo although it could well vary on your system (particularily if you are running
 echo Windows 7 or if you have installed the Java Development Kit.  Within that
@@ -55,28 +124,46 @@ echo folder there should be a folder called 'bin' and the %_JAVAPROG% program wi
 echo be located inside that.
 echo '
 echo If you press ENTER then calibre2opds will hope that the %_JAVAPROG% program can
-echo found on the system search path and will continue on that basis.   If you do not
-echo want to do this then use CTRL-C to abandon trying to start calibre2opds.
-
+echo found on the system search path and will continue on that basis.  
+echo ----------------------------------------------------------------------------
+echo '
 pause
 
-set _JAVACMD=JAVA.EXE
-%JAVAPROG%
-
-:no64bit
-REM not found so hope on search path
 set _JAVACMD=%_JAVAPROG%
 
 :run_c2o
+if exist uninstaller.jar (
+	REM This bit is to work around what seems to be a bug in IzPack which seems to
+	REM set up the uninstall shortcut to use run.cmd regardless of te specification!
+	cd ..
+	"%_JAVACMD%" -jar uninstaller/uninstaller.jar
+	goto end
+)
+echo '
+echo "-----------------------"
+echo " Calibre2Opds STARTING "
+echo "-----------------------"
+echo '
 REM  We set stack limits explicitly here to get consistency across systems
 REM -Xms<value> define starting size
 REM -Xmx<value> defines maximum size
 REM -Xss<value> defines stack size
 REM It is possible that for very large libraries this may not be enough - we will have to see.
+echo [INFO]  "%_JAVACMD%" -Xms128m -Xmx512m -cp OpdsOutput-3.1-SNAPSHOT.jar Cli %*
 "%_JAVACMD%" -Xms128m -Xmx512m -cp OpdsOutput-3.1-SNAPSHOT.jar Cli %*
+echo '
+echo "-----------------------"
+echo " Calibre2Opds FINISHED "
+echo "-----------------------"
+echo '
+pause
 
+:end
+REM Clear down all the environment variables we used
 set _JAVACMD=
 set _JAVAPROG=
+set _C2O=
+set _MYVAR1=
+set _MYVAR2=
 
-pause
 
