f550d910e4acaca1184ff3d5b5cfd2db7c98cd79
itimpi@gmail.com <itimpi@gmail.com@6ab1c9ca-8b3c-d243-0dc9-87497c800d1a>
2012-04-30 15:00:29 +0000

    Updated documentation and release number strings in preparation for 3.1 release

--- trunk/uploadAlphaInstallers.cmd
+++ trunk/uploadAlphaInstallers.cmd
@@ -1,15 +1,15 @@
 @echo off
 set USER=%1
 set PASSWORD=%2
-FOR %%G IN (calibre2opds-3.1-beta-*.zip) DO (
+FOR %%G IN (calibre2opds-3.2-beta-*.zip) DO (
 	echo uploading %%G
 	googlecode_upload.py -u %USER% -w %PASSWORD% -p calibre2opds -s "An archive built from the bleeding edge sources. Use at you own risks, it's not even a beta..." -l "Type-Archive,OpSys-All,Alpha,HEAD" %%G
 )
-FOR %%G IN (calibre2opds-3.1-beta-*.exe) DO (
+FOR %%G IN (calibre2opds-3.2-beta-*.exe) DO (
 	echo uploading %%G
 	googlecode_upload.py -u %USER% -w %PASSWORD% -p calibre2opds -s "An installer built from the bleeding edge sources. Use at you own risks, it's not even a beta..." -l "Type-Installer,OpSys-Windows,Alpha,HEAD" %%G
 )
-FOR %%G IN (calibre2opds-3.1-beta-*.jar) DO (
+FOR %%G IN (calibre2opds-3.2-beta-*.jar) DO (
 	echo uploading %%G
 	googlecode_upload.py -u %USER% -w %PASSWORD% -p calibre2opds -s "An installer built from the bleeding edge sources. Use at you own risks, it's not even a beta..." -l "Type-Installer,OpSys-All,Alpha,HEAD" %%G
 )
