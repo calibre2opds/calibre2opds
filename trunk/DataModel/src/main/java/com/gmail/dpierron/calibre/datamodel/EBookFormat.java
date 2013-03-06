019c802d840650a0754396e09439b32c19a5b714
itimpi <itimpi@6ab1c9ca-8b3c-d243-0dc9-87497c800d1a>
2013-03-06 17:23:46 +0000

    Fix Bug #c20-130 and #c2o-133 Added mime type for PRC, and added AZW3, KF8 and DOCX as known file types

--- trunk/DataModel/src/main/java/com/gmail/dpierron/calibre/datamodel/EBookFormat.java
+++ trunk/DataModel/src/main/java/com/gmail/dpierron/calibre/datamodel/EBookFormat.java
@@ -8,8 +8,10 @@ public class EBookFormat implements Comparable<EBookFormat> {
   public final static EBookFormat EPUB = new EBookFormat("EPUB", "application/epub+zip");
   private final static EBookFormat TXT = new EBookFormat("TXT", "text/plain");
   private final static EBookFormat ZIP = new EBookFormat("ZIP", "application/zip");
-  private final static EBookFormat PRC = new EBookFormat("PRC");
+//  private final static EBookFormat PRC = new EBookFormat("PRC");
+  private final static EBookFormat PRC = new EBookFormat("PRC", "application/x-mobipocket");
   private final static EBookFormat PDB = new EBookFormat("PDB", "application/pdb");
+  private final static EBookFormat AZW = new EBookFormat("AZW", "application/octet-stream");
   private final static EBookFormat MOBI = new EBookFormat("MOBI", "application/x-mobipocket-ebook");
   private final static EBookFormat LRF = new EBookFormat("LRF", "application/x-sony-bbeb");
   private final static EBookFormat LRX = new EBookFormat("LRX", "application/x-bbeb-book");
@@ -19,12 +21,14 @@ public class EBookFormat implements Comparable<EBookFormat> {
   private final static EBookFormat RTF = new EBookFormat("RTF", "text/rtf");
   private final static EBookFormat LIT = new EBookFormat("LIT", "application/x-ms-reader");
   private final static EBookFormat DOC = new EBookFormat("DOC", "application/msword");
+  private final static EBookFormat DOCX = new EBookFormat("DOCX", "application/msword");
   private final static EBookFormat CBR = new EBookFormat("CBR", "application/x-cbr");
   private final static EBookFormat CBZ = new EBookFormat("CBZ", "application/x-cbz");
   private final static EBookFormat CHM = new EBookFormat("CHM", "application/x-chm");
-  private final static EBookFormat AZW = new EBookFormat("AZW", "application/octet-stream");
+  private final static EBookFormat AZW3 = new EBookFormat("AZW3", "application/octet-stream");
+  private final static EBookFormat KF8 = new EBookFormat("KF8", "application/octet-stream");
 
-  private final static EBookFormat[] values = {EPUB, TXT, ZIP, PRC, PDB, AZW, MOBI, LRF, LRX, FB2, RAR, PDF, RTF, LIT, DOC, CBR, CBZ, CHM};
+  private final static EBookFormat[] values = {EPUB, TXT, ZIP, PRC, PDB, AZW, MOBI, LRF, LRX, FB2, RAR, PDF, RTF, LIT, DOC, DOCX, CBR, CBZ, CHM, AZW3, KF8, };
 
   private String mime;
   private final String name;
