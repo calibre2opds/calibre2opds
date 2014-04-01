48b4caf05c6d8b628f4eac2d3ec97bc21190d813
itimpi <itimpi@6ab1c9ca-8b3c-d243-0dc9-87497c800d1a>
2014-04-01 12:04:35 +0000

    All strings updated ready to start the 3.4 beta release program

--- script/run.cmd
+++ script/run.cmd
@@ -15,7 +15,7 @@ cls
 echo Calibre2opds startup
 echo ====================
 
-set _C2O=OpdsOutput-3.3-SNAPSHOT.jar
+set _C2O=OpdsOutput-3.4-SNAPSHOT.jar
 set _CD=%cd%
 echo [INFO] Current Directory: %_CD%
 
