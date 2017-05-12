a519b24875b251fdf343915e9d7114193198cde9
calibre2opds <calibre2opds@gmail.com>
2017-05-12 12:55:05 +0100

    Updates to Lithuanian localization

--- CalibreQueryLanguage/target/generated-sources/antlr3/com/gmail/dpierron/calibre/datamodel/calibrequerylanguage/CalibreQueryParser.java
+++ CalibreQueryLanguage/target/generated-sources/antlr3/com/gmail/dpierron/calibre/datamodel/calibrequerylanguage/CalibreQueryParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 com\\gmail\\dpierron\\calibre\\datamodel\\calibrequerylanguage\\CalibreQueryParser.g 2017-05-04 16:41:21
+// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 com\\gmail\\dpierron\\calibre\\datamodel\\calibrequerylanguage\\CalibreQueryParser.g 2017-05-12 10:51:25
 
 
     package com.gmail.dpierron.calibre.datamodel.calibrequerylanguage;
