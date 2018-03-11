becab43f98c16dc27236671888a9b64ace0df4b3
calibre2opds <calibre2opds@gmail.com>
2018-03-11 11:51:59 +0000

    Fixes for some silly regressions to batch build files

--- CalibreQueryLanguage/target/generated-sources/antlr3/com/gmail/dpierron/calibre/datamodel/calibrequerylanguage/CalibreQueryLexer.java
+++ CalibreQueryLanguage/target/generated-sources/antlr3/com/gmail/dpierron/calibre/datamodel/calibrequerylanguage/CalibreQueryLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 com\\gmail\\dpierron\\calibre\\datamodel\\calibrequerylanguage\\CalibreQueryLexer.g 2018-01-24 10:57:38
+// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 com\\gmail\\dpierron\\calibre\\datamodel\\calibrequerylanguage\\CalibreQueryLexer.g 2018-03-11 11:27:24
 
 
     package com.gmail.dpierron.calibre.datamodel.calibrequerylanguage;
--- CalibreQueryLanguage/target/generated-sources/antlr3/com/gmail/dpierron/calibre/datamodel/calibrequerylanguage/CalibreQueryParser.java
+++ CalibreQueryLanguage/target/generated-sources/antlr3/com/gmail/dpierron/calibre/datamodel/calibrequerylanguage/CalibreQueryParser.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 com\\gmail\\dpierron\\calibre\\datamodel\\calibrequerylanguage\\CalibreQueryParser.g 2018-01-24 10:57:39
+// $ANTLR 3.1.3 Mar 17, 2009 19:23:44 com\\gmail\\dpierron\\calibre\\datamodel\\calibrequerylanguage\\CalibreQueryParser.g 2018-03-11 11:27:25
 
 
     package com.gmail.dpierron.calibre.datamodel.calibrequerylanguage;
